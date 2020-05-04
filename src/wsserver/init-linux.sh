41d785dd6bb45548333d53e8545d008a46339554
ppshuai <ppshuai@localhost.localdomain>
2020-05-04 13:21:35 +0800

    修复linux下脚本

--- src/wsserver/init-linux.sh
+++ src/wsserver/init-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#cp linux/libsqlite3.a $HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/lib/ -f
+cp linux/libsqlite3.a $HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/lib/ -f
 cargo install diesel_cli --no-default-features --features sqlite --force
 echo "DATABASE_URL=test.db" > .env
 diesel migration run
